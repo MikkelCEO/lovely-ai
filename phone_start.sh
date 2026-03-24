f747cbb83838c458914f6c5fe75e3627a1e0ea83
UGREEN USER <mikkel.ceo@debian.dedomain>
2026-03-24 22:26:41 +0100

    auto update

--- phone_start.sh
+++ phone_start.sh
@@ -9,18 +9,3 @@ git pull
 pip install -r phone_requirements.txt --break-system-packages
 
 # Start auto-update loop
-nohup bash -c '
-cd /volume1/Projects/ai-chat/Phone
-while true; do
-  git fetch
-  LOCAL=$(git rev-parse HEAD)
-  REMOTE=$(git rev-parse origin/main)
-
-  if [ "$LOCAL" != "$REMOTE" ]; then
-    echo "Updating code..."
-    git pull
-  fi
-
-  sleep 2
-done
-' > gitpull.log 2>&1 &
