d13f93f0029ffd9337b451dd5a8d66ff60541c87
UGREEN USER <mikkel.ceo@debian.dedomain>
2026-03-25 18:37:47 +0100

    auto update

--- phone_test.py
+++ phone_test.py
@@ -12,8 +12,8 @@ except ImportError:
 # === CONFIG ===
 ACCOUNT_SID = "AC0c94c6664032a772dbec729dba49ecb9"   # your Account SID
 AUTH_TOKEN = "070adb4a65fe8a1d879bcc862439ba1e"                 # put your real Auth Token
-TWILIO_NUMBER = "+4915888654546"                    # your Twilio number
-YOUR_PHONE = "+18392616244"                        # the number you want to call
+TWILIO_NUMBER = "+18392616244"                    # your Twilio number
+YOUR_PHONE = "+4915888654546"                        # the number you want to call
 
 client = Client(ACCOUNT_SID, AUTH_TOKEN)
 
